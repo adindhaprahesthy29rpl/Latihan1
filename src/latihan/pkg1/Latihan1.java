31f335db14b1fd31b6601de2d2ce6c0042563627
adindhaprahesthy29rpl <adindha_prahesthy_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 14:11:44 +0700

    Latihan 1

---
+++ src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel dan Menyimpan nilai
+        int bil = 1;
+        
+        // Perulangan Do 
+        do{
+            //Statement
+                    System.out.print(bil+" ");
+                    bil++;
+                    
+        // Perulangan While
+        // Akan berulang sampai bilangan kurang dari atau sama dengan 10
+        }while (bil<=10);
+        }
+    }
+    
+
